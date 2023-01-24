38730850adc1a33c82d86c04a13c1fede3e427cb
Bilal Khilji <kbinfo4u@gmail.com>
2023-01-24 17:32:26 +0530

    Initialize project using Create React App

--- src/Library/Constants.js
+++ src/Library/Constants.js
@@ -1,12 +1,12 @@
 export const Constants = {
     COMPANY: {
         NAME: "SpectrumSoft",
-        ADDRESS: "",
+        ADDRESS: "1st Floor, Ganeshyam Plaza, Near Sunita TVS, Pratap Nagar Road, Akhliya, Jodhpur Rajasthan - 342003",
         LOCALITY: "Jodhpur",
         REGION: "Rajasthan",
-        POSTAL: "342001",
+        POSTAL: "342003",
         COUNTRY: "India",
-        CONTACT: "",
-        EMAIL: ""
+        CONTACT: "7877862786, 02913563719",
+        EMAIL: "support@spectrumsoft.in"
     },
 }
